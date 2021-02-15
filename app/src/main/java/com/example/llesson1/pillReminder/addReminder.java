26e3599753389a766a526a8cf5973eb2a948961e
joyleen <joyleenlyneth@gmail.com>
2021-02-16 06:30:22 +0700

    3rd

---
+++ app/src/main/java/com/example/llesson1/pillReminder/addReminder.java
@@ -0,0 +1,18 @@
+package com.example.llesson1.pillReminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.llesson1.R;
+
+public class addReminder extends AppCompatActivity {
+
+    public static final int RE
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+    }
+}

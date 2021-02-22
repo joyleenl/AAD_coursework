3cd32372f0669db4282fae7a2d951a79eb5972d1
joyleen <joyleenlyneth@gmail.com>
2021-02-22 11:49:54 +0700

    commit after

--- app/src/main/java/com/example/llesson1/pillReminder/BasePresenter.java
+++
@@ -1,7 +0,0 @@
-package com.example.llesson1.pillReminder;
-
-
-public interface BasePresenter {
-
-    void start();
-}
---
+++ app/src/main/java/com/example/llesson1/pillReminder/alarm/ReminderAlarmService.java
@@ -0,0 +1,75 @@
+package com.example.llesson1.pillReminder.alarm;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.llesson1.R;
+import com.example.llesson1.pillReminder.ReminderActivity;
+import com.example.llesson1.pillReminder.data.AlarmReminderContract;
+
+public class ReminderAlarmService extends IntentService {
+    private static final String TAG = ReminderAlarmService.class.getSimpleName();
+
+    private static final int NOTIFICATION_ID = 42;
+
+    Cursor cursor;
+    //This is a deep link intent, and needs the task stack
+    public static PendingIntent getReminderPendingIntent(Context context, Uri uri) {
+        Intent action = new Intent(context, ReminderAlarmService.class);
+        action.setData(uri);
+        return PendingIntent.getService(context, 0, action, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    public ReminderAlarmService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Uri uri = intent.getData();
+
+        //Display a notification to view the task details
+        Intent action = new Intent(this, ReminderActivity.class);
+        action.setData(uri);
+        PendingIntent operation = TaskStackBuilder.create(this)
+                .addNextIntentWithParentStack(action)
+                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //Grab the task description
+        if(uri != null){
+            cursor = getContentResolver().query(uri, null, null, null, null);
+        }
+
+        String description = "";
+        try {
+            if (cursor != null && cursor.moveToFirst()) {
+                description = AlarmReminderContract.getColumnString(cursor, AlarmReminderContract.AlarmReminderEntry.KEY_TITLE);
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+        Notification note = new NotificationCompat.Builder(this)
+                .setContentTitle(getString(R.string.add_reminder))
+                .setContentText(description)
+                .setSmallIcon(R.drawable.ic_baseline_add_24)
+                .setContentIntent(operation)
+                .setAutoCancel(true)
+                .build();
+
+        manager.notify(NOTIFICATION_ID, note);
+    }
+}
